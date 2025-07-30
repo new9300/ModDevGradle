6904e71fb5fb07938fda0eb565889d499db929d3
new9300 <104883028+new9300@users.noreply.github.com>
2025-07-31 00:18:39 +0800

    Delete src/java8/java/net/neoforged/moddevgradle/boot directory

--- src/java8/java/net/neoforged/moddevgradle/boot/LegacyRepositoriesPlugin.java
+++
@@ -1,9 +0,0 @@
-package net.neoforged.moddevgradle.boot;
-
-import org.gradle.api.plugins.PluginAware;
-
-public class LegacyRepositoriesPlugin extends TrampolinePlugin<PluginAware> {
-    public LegacyRepositoriesPlugin() {
-        super("net.neoforged.moddevgradle.legacyforge.internal.LegacyRepositoriesPlugin");
-    }
-}
--- src/java8/java/net/neoforged/moddevgradle/boot/RepositoriesPlugin.java
+++
@@ -1,9 +0,0 @@
-package net.neoforged.moddevgradle.boot;
-
-import org.gradle.api.plugins.PluginAware;
-
-public class RepositoriesPlugin extends TrampolinePlugin<PluginAware> {
-    public RepositoriesPlugin() {
-        super("net.neoforged.moddevgradle.internal.RepositoriesPlugin");
-    }
-}
