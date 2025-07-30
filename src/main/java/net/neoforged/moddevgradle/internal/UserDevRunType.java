61c130aaefcb8741301486fa364c5cb3b592fa5a
new9300 <104883028+new9300@users.noreply.github.com>
2025-07-31 00:19:48 +0800

    Delete src/main/java/net/neoforged directory

--- src/main/java/net/neoforged/moddevgradle/internal/UserDevRunType.java
+++
@@ -1,7 +0,0 @@
-package net.neoforged.moddevgradle.internal;
-
-import java.util.List;
-import java.util.Map;
-
-public record UserDevRunType(boolean singleInstance, String main, List<String> args, List<String> jvmArgs,
-        Map<String, String> env, Map<String, String> props) {}
